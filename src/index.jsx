e1210f74803c5d2f10a6086f4eeb9675bb7a880c
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-09-26 09:38:05 -0700

    Add BaaS.

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import persistDataLocally from './middleware/persist-data-locally';
 import ticketListReducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
 
-const store = createStore(rootReducer);
+let retrievedState;
+try {
+  retrievedState = localStorage.getItem('reduxStore');
+  if (retrievedState === null){
+    retrievedState = {};
+  }
+  retrievedState = JSON.parse(retrievedState);
+} catch (err){
+  retrievedState = {};
+}
+
+const store = createStore(rootReducer, retrievedState, applyMiddleware(persistDataLocally));
 
 let unsubscribe = store.subscribe(() =>
   console.log(store.getState())
